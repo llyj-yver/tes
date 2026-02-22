2128b4eecdf81cb6ef60318811df88af9f279ef0
Soft_Engr.Jyll <jyllyvers@gmail.com>
2026-02-23 05:08:03 +0800

    nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ModuleProvider>
+          {children}
+        </ModuleProvider>
       </body>
     </html>
   );
